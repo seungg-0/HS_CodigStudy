50e85f3a5b5014212d6456872737deb7134495aa
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2021-06-25 12:45:51 +0900

    Update LeetCode_P14.py

--- LeetCode_P14.py
+++ LeetCode_P14.py
@@ -3,16 +3,20 @@ class Solution:
         list_len_sep = []
         result_list = []
         result = ""
-
-        if (len(strs) == 1) and (len(strs[0]) != 0):
+        
+        # 리스트에 문자열이 하나 존재하는 경우
+        if (len(strs) == 1) and (len(strs[0]) != 0): 
             return strs[0]
 
+        # 문자열의 길이가 담긴 list 생성
         for i in range(0, len(strs)):
             list_len_sep.append(len(strs[i]))
             
+        # "" 문자열이 포함된 경우
         if min(list_len_sep) == 0:
             return ""
 
+        # 이외의 경우
         for i in range(0, min(list_len_sep)):
             lis = []
             for j in range(0, len(strs)):
@@ -20,13 +24,13 @@ class Solution:
             del_dupl = set(lis)
             result_list.append(list(del_dupl))
 
+        # output 생성
         for i in range(0, len(result_list)):
             if len(result_list[0]) > 1:
                 return ""
 
             elif len(result_list[i]) == 1:
                 result += str(result_list[i][0])
-                if i == (len(result_list) - 1):
-                    return result
             else:
-                return result
+                break
+        return result
