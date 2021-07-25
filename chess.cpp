49d52e95465f14ec9b23ce3b06ccc88fd053c32e
Hyoseok Jang <dddpower@gamil.com>
2021-07-25 19:06:15 +0900

    updated validate_move, check_ending

--- chess.cpp
+++ chess.cpp
@@ -30,20 +30,42 @@ void print_board() {
 }
 
 
-void process_move(const string& input1, const string& input2) {
-  // e.g : a3 b3
-  int col_1 = input1[0] - 'a';
-  int row_1 = input1[1] - '1';
-  int col_2 = input2[0] - 'a';
-  int row_2 = input2[1] - '1';
-  // cout << "before test" << endl;
-  // cout << col_1 << endl;
-  // cout << row_1 << endl;
-  // cout << col_2 << endl;
-  // cout << row_2 << endl;
-  // cout << "after test" << endl;
-  board[row_2][col_2] = board[row_1][col_1];
-  board[row_1][col_1] = '-';
+// For now, only king exists.
+bool validate_move(int col1, int row1, int col2, int row2) {
+  auto check_value = [](int val) -> bool {
+    return 0 <= val && val < board_size;
+  };
+  auto check_boundary = 
+    [&check_value](int col1, int row1, int col2, int row2) -> bool {
+    return check_value(col1) && check_value(row1) &&
+            check_value(col2) && check_value(row2);
+  };
+  if (!check_boundary(col1, row1, col2, row2)) {
+    return false;
+  }
+  if (col1 == col2 && row1 == row2) {
+    return false;
+  }
+  if (abs(col1 - col2) > 1 || abs(row1 - row2) > 1) {
+    return false;
+  }
+  return true;
+}
+
+// 1. validate move
+// 2. check king's status (to see who win the game)
+bool process_move(int col1, int row1, int col2, int row2) {
+  if (board[row2][col2] == 'B') {
+    cout << "White win!\n";
+    return true;
+  }
+  if (board[row2][col2] == 'W') {
+    cout << "Black win!\n";
+    return true;
+  }
+  board[row2][col2] = board[row1][col1];
+  board[row1][col1] = '-';
+  return false;
 }
 void play() {
   cout << "press q to quit" << endl;
@@ -54,7 +76,16 @@ void play() {
   if (input1[0] == 'q')
     return;
   cin >> input2;
-  process_move(input1, input2);
+  int col1 = input1[0] - 'a';
+  int row1 = input1[1] - '1';
+  int col2 = input2[0] - 'a';
+  int row2 = input2[1] - '1';
+  if (!validate_move(col1, row1, col2, row2)) {
+    cout << "Invalid input. Please reinput command.\n";
+  } else {
+    if (process_move(col1, row1, col2, row2))
+      return;
+  }
   return play();
 }
 int main() {
