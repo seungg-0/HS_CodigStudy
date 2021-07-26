559a5bdf59349a372c437c11f4bbe6a439d5d980
seungg-0 <61819350+seungg-0@users.noreply.github.com>
2021-07-26 11:53:30 +0900

    Update chess_yet.py

--- chess_yet.py
+++ chess_yet.py
@@ -1,20 +1,33 @@
 class Chess():
-    input_to_chessboard = {'a':0, 'b':1, 'c': 2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, '1':7, '2':6, '3':5, '4':4, '5':3, '6':2, '7':1, '8':0}
     def __init__(self):
+        self.input_to_chessboard = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, '1': 7, '2': 6, '3': 5,'4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+
+    def GameStart(self):
         print("Let's start !")
         self.chessboard = [["ㅁ"]*8 for _ in range(8)]
-        self.chessboard[8][4] = "WK"
+        self.chessboard[7][4] = "WK"
         self.chessboard[0][4] = "BK"
-        print(self.chessboard)
+
+        #체스보드 2차원으로 출력
+        i = 0
+        while i < len(self.chessboard):  # 세로 크기
+            j = 0
+            while j < len(self.chessboard[i]):  # 가로 크기
+                print(self.chessboard[i][j], end=' ')
+                j += 1  # 가로 인덱스를 1 증가시킴
+            print()
+            i += 1  # 세로 인덱스를 1 증가시킴
+
         # BeforeMove is the variable that 'before move'
         self.BeforeMoveWhite = "e1"
         self.BeforeMoveBlack = "e8"
         self.PrintFirst()
-        self.SelectFirst()
+
 
     def PrintFirst(self):
         print("Which color starts first? (W or B)")
         self.start = input()
+        self.SelectFirst()
 
     def SelectFirst(self):
         if self.start == "W":
@@ -37,28 +50,81 @@ class Chess():
         self.BlackMove()
 
     def WhiteMove(self):
-        if self.WhiteInput[1] == self.beforeMoveWhite[0]:
-            if int(self.WhiteInput[2]) == 1:
-                self.MoveUp()
-            elif int(self.WhiteInput[2]) == 8:
-                self.MoveDown()
-            if int(self.WhieInput[2]) == (int(self.beforeMoveWhite[1]) - 1):
-        elif self.WihteInput[1] == self.beforeMoveWhite[0] - 1
+        self.Wrow = self.WhiteInput[2]
+        self.Wcolumn = self.WhiteInput[1]
+        if self.Wrow == self.BeforeMoveBlack[1] and self.Wcolumn == self.BeforeMoveBlack[0]:
+            print("White Win !!!")
+            return 0
+        else:
+            self.WhitePrint()
+        self.BlackMove()
 
     def BlackMove(self):
+        self.Brow = self.BlackInput[2]
+        self.Bcolumn = self.BlackInput[1]
+        if self.Brow == self.BeforeMoveWhite[1] and self.Bcolumn == self.BeforeMoveWhite[0]:
+            print("Black Win !!!")
+            return 0
+        else:
+            self.BlackPrint()
+        self.WhiteMove()
 
-    def MoveUp(self):
+    def WhitePrint(self):
+        self.Brow = self.BeforeMoveBlack[1]
+        self.Bcolumn = self.BeforeMoveBlack[0]
+        self.chessboard = [["ㅁ"] * 8 for _ in range(8)]
+        self.chessboard[self.input_to_chessboard[self.Wrow]][self.input_to_chessboard[self.Wcolumn]] = "WK"
+        self.chessboard[self.input_to_chessboard[self.Brow]][self.input_to_chessboard[self.Bcolumn]] = "BK"
 
-    def MoveDown(self):
+        #체스보드 2차원으로 출력
+        i = 0
+        while i < len(self.chessboard):  # 세로 크기
+            j = 0
+            while j < len(self.chessboard[i]):  # 가로 크기
+                print(self.chessboard[i][j], end=' ')
+                j += 1  # 가로 인덱스를 1 증가시킴
+            print()
+            i += 1  # 세로 인덱스를 1 증가시킴
 
-    def MoveLeft(self):
+        self.BeforeMoveWhite = self.Wcolumn + self.Wrow
 
-    def MoveRight(self):
+    def BlackPrint(self):
+        self.Wrow = self.BeforeMoveWhite[1]
+        self.Wcolumn = self.BeforeMoveWhite[0]
+        self.chessboard = [["ㅁ"] * 8 for _ in range(8)]
+        self.chessboard[self.input_to_chessboard[self.Wrow]][self.input_to_chessboard[self.Wcolumn]] = "WK"
+        self.chessboard[self.input_to_chessboard[self.Brow]][self.input_to_chessboard[self.Bcolumn]] = "BK"
 
-    def MoveLeftUp(self):
+        #체스보드 2차원으로 출력
+        i = 0
+        while i < len(self.chessboard):  # 세로 크기
+            j = 0
+            while j < len(self.chessboard[i]):  # 가로 크기
+                print(self.chessboard[i][j], end=' ')
+                j += 1  # 가로 인덱스를 1 증가시킴
+            print()
+            i += 1  # 세로 인덱스를 1 증가시킴
 
-    def MoveRightUp(self):
+        self.BeforeMoveBlack = self.Bcolumn + self.Brow
 
-    def MoveLeftDown(self):
+    # def MoveUp(self):
+    #
+    # def MoveDown(self):
+    #
+    # def MoveLeft(self):
+    #
+    # def MoveRight(self):
+    #
+    # def MoveLeftUp(self):
+    #
+    # def MoveRightUp(self):
+    #
+    # def MoveLeftDown(self):
+    #
+    # def MoveRightDown(self):
 
-    def MoveRightDow(self):
+run = Chess()
+game = run.GameStart()
+
+
+# 29행 에러 해결, 가장자리에 위치하는 경우 예외처리, 체크메이트 추가 
